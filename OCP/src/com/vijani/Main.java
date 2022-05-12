8a949d4b6d4576d4aa82f486f8711309f6851360
vijanipiyawardana <vijanipiyawardana@gmail.com>
2022-05-12 16:31:31 +0200

    first project added: javadoc, jar

---
+++ OCP/src/com/vijani/Main.java
@@ -0,0 +1,4 @@
+package com.vijani;
+
+public class Main {
+}
---
+++ OCP/src/com/vijani/entity/Animal.java
@@ -0,0 +1,36 @@
+package com.vijani.entity;
+
+/**
+ * This is a simple Java Bean Class
+ * @author Vijani Piyawardana
+ * @version 17
+ */
+public class Animal {
+
+    // field
+    private String name;
+
+    /**
+     * get the name of animal
+     * @return name of animal
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * set the name of animal
+     * @param name - name of animal
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * run the program
+     * @param args
+     */
+    public static void main(String args[]){
+        System.out.println("Welcome!!!");
+    }
+}
